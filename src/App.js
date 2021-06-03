d95983f0bc6f5c218e6829fd20745804103889cb
Grzegorz Kulesza <kuleszagr@gmail.com>
2021-06-03 18:35:41 +0200

    add data as an array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {title: 'Car Insurence', amount: 294.54, date: new Date(2021, 2, 28)}
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
 }
 
 export default App;
-
-//Source: https://github.com/academind/react-complete-guide-code/tree/03-react-basics-working-with-components/code
