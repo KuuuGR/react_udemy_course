15617149068b5aaf2de869bfd710e64e8068b067
Grzegorz Kulesza <kuleszagr@gmail.com>
2021-06-03 22:25:18 +0200

    change open - closed elements for self-closing elements add new component with styling

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,18 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+    const year = props.date.getFullYear();
+    const month = props.date.toLocaleString('en-GB', { month: 'long' });
+    const day = props.date.toLocaleString('en-GB', { day: '2-digit' });
+
+    return (
+        <div>
+            <div>{year}</div>
+            <div>{month}</div>
+            <div>{day}</div>
+        </div>
+    );
+
+}
+
+export default ExpenseDate;
