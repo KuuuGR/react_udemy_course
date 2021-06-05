5fb3e1d08c75bc504ccfc00c21365cb6f843d6dc
Grzegorz Kulesza <kuleszagr@gmail.com>
2021-06-05 20:39:13 +0200

    back to first approach to save multiple states in one component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,55 +3,53 @@ import React, { useState } from 'react';
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle ] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate,setEnteredDate] = useState('');
-    const [userInput, steUserInput] = useState({
-        enteredTitle: '',
-        setEnteredAmount: '',
-        setEnteredDate: ''
-    });
+    const [enteredTitle, setEnteredTitle ] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate,setEnteredDate] = useState('');
 
+    // const [userInput, steUserInput] = useState({
+    //     enteredTitle: '',
+    //     setEnteredAmount: '',
+    //     setEnteredDate: ''
+    // });
 
     const titleChangeHandler = (event) => {
-        //setEnteredTitle(event.target.value);
+        setEnteredTitle(event.target.value);
         
         // steUserInput({
         // ...userInput,
         // enteredTitle: event.target.value,
         // });
 
-        steUserInput((prevState) => {
-            return {...prevState, enteredTitle: event.target.value};
-        });
+        // steUserInput((prevState) => {
+        //     return {...prevState, enteredTitle: event.target.value};
+        // });
     };
 
     const amountChangeHandler = (event) => {
-        //setEnteredAmount(event.target.value);
+        setEnteredAmount(event.target.value);
 
         // steUserInput({
         //     ...userInput,
         //     enteredAmmount: event.target.value,
         //     });
 
-        steUserInput((prevState) => {
-            return {...prevState, enteredAmount: event.target.value};
-        });
+        // steUserInput((prevState) => {
+        //     return {...prevState, enteredAmount: event.target.value};
+        // });
     };
 
     const dateChangeHandler = (event) => {
-        //setEnteredDate(event.target.value);
+        setEnteredDate(event.target.value);
         
         // steUserInput({
         //     ...userInput,
         //     entedDate: event.target.value,
         //     });
 
-        steUserInput((prevState) => {
-            return {...prevState, enteredDate: event.target.value};
-        });
-
-
+        // steUserInput((prevState) => {
+        //     return {...prevState, enteredDate: event.target.value};
+        // });
     };
 
     return <form>
@@ -59,17 +57,30 @@ const ExpenseForm = () => {
             
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input 
+                type='text' 
+                onChange={titleChangeHandler} 
+                />
             </div>
 
             <div className='new-expense__control'>
                 <label>amount</label>
-                <input type='number' min ="0.01" step="0.01" onChange={amountChangeHandler} />
+                <input 
+                type='number' 
+                min ="0.01" 
+                step="0.01" 
+                onChange={amountChangeHandler} 
+                />
             </div>
 
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                <input 
+                type='date' 
+                min="2019-01-01" 
+                max="2022-12-31" 
+                onChange={dateChangeHandler} 
+                />
             </div>
 
             <div className="new-expense__actions">
